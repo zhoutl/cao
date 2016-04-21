3e9e0ad1061bc6e3afe6481280f35bb1e3bd0ad5
zhoutl <736979600@qq.com>
2016-04-21 18:37:31 +0800

    2016042102

---
+++ Application/Wap/Controller/ItemController.class.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016-04-21
+ * Time: 16:40
+ */
+
+namespace Wap\Controller;
+
+
+use Think\Controller;
+
+class ItemController extends  Controller
+{
+
+    public function  detail(){
+
+        $this->display();
+    }
+}
